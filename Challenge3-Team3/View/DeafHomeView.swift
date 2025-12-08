4224d7963a21906b0168b36a0beae0d10de2d3c5
Alya Alabdulrahim <AliaKhalidAlabdulrahim@gmail.com>
2025-12-08 14:50:06 +0300

    navigation messages

--- Challenge3-Team3/View/DeafHomeView.swift
+++ Challenge3-Team3/View/DeafHomeView.swift
@@ -149,14 +149,8 @@ struct HeaderView: View {
 
             HStack (spacing: 24) {
                 NavigationLink {
-//                    MessagesView()
-                    LiveChatView(
-                        currentUserId: "user123",
-                        currentUserName: "Me",
-                        recipientUserId: "user456",
-                        recipientName: "John Doe",
-                        recipientContact: "+966501234567"
-                    )
+                    MessagesView()
+              
 
                     
                 } label: {
